b4d3e6f5f7bb4834df7c5c4b5e96eb845416006c
Rajesh G <92634862+Rajesh2723@users.noreply.github.com>
2022-09-24 07:38:06 +0530

    Create RecurevlyRemoveDuplicates.cpp

---
+++ strings/RecurevlyRemoveDuplicates.cpp
@@ -0,0 +1,16 @@
+class Solution{
+public:
+    string rremove(string s){
+        // code here
+        string ans;
+        int n=s.length();
+        for(int i=0;i<s.length();i++){
+            if(i>0 && (s[i-1]==s[i])|| (i+1 <s.length())&& s[i+1]==s[i]){
+                continue;
+            }else{
+                ans+=s[i];
+            }
+        }
+        return ans.length()==s.length()?ans:rremove(ans);
+    }
+};
