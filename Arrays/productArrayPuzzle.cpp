706d30ebc38f3066c8ed1c21cfb5e7b3331f4c1d
Rajesh G <92634862+Rajesh2723@users.noreply.github.com>
2022-10-26 00:54:51 +0530

    Create productArrayPuzzle.cpp

---
+++ Arrays/productArrayPuzzle.cpp
@@ -0,0 +1,40 @@
+/*
+Given an array nums[] of size n, construct a Product Array P (of same size n) such that P[i] is equal to the product of all the elements of nums except nums[i].
+
+ 
+
+Example 1:
+
+Input:
+n = 5
+nums[] = {10, 3, 5, 6, 2}
+Output:
+180 600 360 300 900
+Explanation: 
+For i=0, P[i] = 3*5*6*2 = 180.
+For i=1, P[i] = 10*5*6*2 = 600.
+For i=2, P[i] = 10*3*6*2 = 360.
+For i=3, P[i] = 10*3*5*2 = 300.
+For i=4, P[i] = 10*3*5*6 = 900.
+*/
+class Solution{
+  public:
+    // nums: given vector
+    // return the Product vector P that hold product except self at each index
+    vector<long long int> productExceptSelf(vector<long long int>& nums, int n) {
+       
+      vector<long long int>ans;
+      for(int i=0;i<n;i++){
+          long long int temp=1;
+          for(int j=0;j<n;j++){
+              if(j==i){
+                  continue;
+              }else{
+                  temp=temp*nums[j];
+              }
+          }
+          ans.push_back(temp);
+      }
+      return ans;
+    }
+};
